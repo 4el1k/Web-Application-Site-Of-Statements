7f373d988f77e86c675888af6587a230c975da69
4el1k <salavatalyukov882@gmail.com>
2023-10-30 02:23:54 +0400

    add

---
+++ Site-Of-Statements/src/main/java/ru/itis/controllers/servlets/PostingServlet.java
@@ -0,0 +1,8 @@
+package ru.itis.controllers.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+// ToDo: jsp form, ota, service, post method
+// in future will be implementation another methods of HttpServlet. For example i want to do post edit features.
+public class PostingServlet extends HttpServlet {
+}
